fe7c7195c016ab19737766c7e54ae928b3d8ca14
unknown <ryanrafi72@gmail.com>
2025-05-18 23:49:55 +0600

    fix test scripts

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,9 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseService } from 'src/database/database.service';
+import { UsersService } from 'src/users/users.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +11,12 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        DatabaseService,
+        JwtService,
+        UsersService
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
